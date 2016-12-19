c87dfbdd3706ef348be38069508a50d31eab2668
LucChoubert <luc.choubert@gmail.com>
2016-12-19 22:37:56 +0100

    add mongo connection

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,26 @@
+import os
+import pymongo
 from flask import Flask
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World testPythonBackend!"
+
+@application.route("/listFilms")
+def listFilms():
+	#setup the connection
+        conn = pymongo.Connection(os.environ['OPENSHIFT_NOSQL_DB_URL'])
+        db = conn.parks
+ 
+        #query the DB for all the parkpoints
+        result = db.parkpoints.find()
+ 
+	#Now turn the results into valid JSON
+        return str(json.dumps({'results':list(result)},default=json_util.default))
+    	#return "my list of films!"
+
 
 if __name__ == "__main__":
     application.run()
